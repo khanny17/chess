8b9f177d8fced668a4024a886ed3dc8757fd8913
Kevin <Kph1958@rit.edu>
2013-10-23 01:23:47 -0400

    now empty spaces cannot be "selected" except for a piece to be moved to

--- Chess/src/game/SpaceClickListener.java
+++ Chess/src/game/SpaceClickListener.java
@@ -16,63 +16,70 @@ public class SpaceClickListener extends MouseAdapter {
 	 */
 	@Override
 	public void mouseReleased(MouseEvent e) {		
+
+
 		//get the space which was clicked and the piece on that space
 		Space newSpace = (Space)e.getSource();
 		Piece newPiece = newSpace.getPiece();
 		
-		try {
-			
-			Piece currentPiece = currentSpace.getPiece();
-
-			if(newSpace != currentSpace) {
-
-				//If the space clicked on is empty...
-				if(newPiece == null) {
-
-
-					//Test for legal move
-
-					//move current space piece to new space
-					currentSpace.setPiece(null);
-					newSpace.setPiece(currentPiece);
-
-
-
-				} else { //If the space clicked on is occupied...
-
-
-					//Test for legal move
-
-					//move current space piece to new space
-					currentSpace.setPiece(null);
-					newSpace.setPiece(currentPiece);
-
-
-
-
-				}
+		//if nothing selected, just select the new space and return
+		if(currentSpace == null) {
+			//If the new space is empty don't select it
+			if(newPiece != null) {
+				currentSpace = newSpace;
+				currentSpace.select();
 			}
-
-
-			/*
-			 * Update graphics
-			 */
-
-			//repaint spaces
-			currentSpace.paintComponent(currentSpace.getGraphics());
-			newSpace.paintComponent(newSpace.getGraphics());
-			
-			/*
-			 * Select new space, deselect the old.
-			 * This is where we update the current space to be the new one
-			 */
-			currentSpace.deselect();
-			currentSpace = null;
-			
-		//If current space is null, just select the new space.
-		} catch(NullPointerException n) { 
-			currentSpace = newSpace;
-			currentSpace.select();
+			return;
 		}
+
+
+
+		Piece currentPiece = currentSpace.getPiece();
+
+		if(newSpace != currentSpace) {
+
+			//If the space clicked on is empty...
+			if(newPiece == null) {
+
+
+
+				//Test for legal move
+
+				//move current space piece to new space
+				currentSpace.setPiece(null);
+				newSpace.setPiece(currentPiece);
+
+
+
+			} else { //If the space clicked on is occupied...
+
+
+				//Test for legal move
+
+				//move current space piece to new space
+				currentSpace.setPiece(null);
+				newSpace.setPiece(currentPiece);
+
+
+
+
+			}
+		}
+
+
+		/*
+		 * Update graphics
+		 */
+
+		//repaint spaces
+		currentSpace.paintComponent(currentSpace.getGraphics());
+		newSpace.paintComponent(newSpace.getGraphics());
+
+		/*
+		 * Deselect the old space, set current space to null
+		 */
+		currentSpace.deselect();
+		currentSpace = null;
+
 	}
 }
