4b3e84f4a94ebdd48fc2ff726e5d6513c51fedfa
Kevin <Kph1958@rit.edu>
2013-10-23 01:13:51 -0400

    Made game window non resizable

--- Chess/src/game/GameFrame.java
+++ Chess/src/game/GameFrame.java
@@ -9,15 +9,21 @@ import javax.swing.JFrame;
 @SuppressWarnings("serial")
 public class GameFrame extends JFrame {
 
+	Board board;
+	
 	/**
 	 *	Default constructor for the main game window
 	 */
 	public GameFrame() {
 		setTitle("Chess");
 		getContentPane().setSize(Board.ROWS*64,Board.COLS*64);
-		Board board = new Board();
+		board = new Board();
 		setLayout(board);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		setResizable(false);
+		//If you want to change it to be resizeable, try component listener
+		
 		//Add the spaces to the frame
 		for(int row = 0; row < Board.ROWS; row++) {
 			for(int col = 0; col < Board.COLS; col++) {
